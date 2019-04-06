184878e2947b1d425e00cb81f3a2e8bc5ddf9ad8
qianlidongfeng <root@ubuntu.ubuntu-domain>
2019-04-06 20:31:50 +0800

    update loger.go

--- loger.go
+++ loger.go
@@ -4,38 +4,59 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
+	"sync"
 )
 
+var mu sync.Mutex
 func init(){
 	log.SetFlags(log.Llongfile|log.Ltime|log.Ldate)
 }
 
 func Warn(v ...interface{}){
+	var buf [1024]byte
+	n:=runtime.Stack(buf[:],false)
+	stack := string(buf[:n-1])
+	mu.Lock()
 	log.SetPrefix("[warning]")
-	log.Output(2,fmt.Sprint(v...))
+	s:=fmt.Sprint(v...)
+	log.Output(2,fmt.Sprint(s,"\n[stack]:\n",stack))
+	mu.Unlock()
 }
 
 func Fatal(v ...interface{}){
+	var buf [1024]byte
+	n:=runtime.Stack(buf[:],true)
+	stack := string(buf[:n-1])
+	mu.Lock()
 	log.SetPrefix("[fatal]")
-	log.Output(2,fmt.Sprint(v...))
+	s:=fmt.Sprint(v...)
+	log.Output(2,fmt.Sprint(s,"\n[stack]:\n",stack))
+	mu.Unlock()
 	os.Exit(1)
 }
 
 func Panic(v ...interface{}){
 	s:=fmt.Sprint(v...)
+	mu.Lock()
 	log.SetPrefix("[panic]")
 	log.Output(2,s)
 	panic(s)
+	mu.Unlock()
 }
 
 func Print(v ...interface{}){
 	s:=fmt.Sprint(v...)
+	mu.Lock()
 	log.SetPrefix("")
 	log.Output(2,s)
+	mu.Unlock()
 }
 
 func Debug(v ...interface{}){
 	s:=fmt.Sprint(v...)
-	log.SetPrefix("")
+	mu.Lock()
+	log.SetPrefix("[debug]")
 	log.Output(2,s)
+	mu.Unlock()
 }
